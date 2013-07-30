0a30fbc6083ce03215f3bce136e48fab495471ee
Ben Willis <benjamin.willis@gmail.com>
2013-07-29 23:02:49 -0700

    Fixing mo' problems

--- lib/so_informed/foursquare/checkin.rb
+++ lib/so_informed/foursquare/checkin.rb
@@ -1,17 +1,18 @@
 module SoInformed
   module Foursquare
     class Checkin
+
       attr_reader :username, :user_id, :venue_name, :address, :shout
+
       def initialize(data)
-        @username     = "#{checkin['user']['firstName']} #{checkin['user']['lastName']}"
-        @user_id      = checkin['user']['id']
-        @venue_name   = checkin['venue']['name']
+        @username     = "#{data['user']['firstName']} #{data['user']['lastName']}"
+        @user_id      = data['user']['id']
+        @venue_name   = data['venue']['name']
+        @shout        = data['shout']
 
-        address       = checkin['venue']['location']['address']
-        cross_street  = checkin['venue']['location']['crossStreet']
+        address       = data['venue']['location']['address']
+        cross_street  = data['venue']['location']['crossStreet']
         @address = " (#{address}#{cross_street ? " by #{cross_street}" : ""})" if address
-
-        @shout        = checkin['shout']
       end
 
       def has_shout?
