e80650cd8cb3406272a323bfe14c59fc5d0a91f1
Ben Willis <benjamin.willis@gmail.com>
2013-07-31 00:08:11 -0700

    Added privacy policy and changed session cookie to a better name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-FoursquareSpeed::Application.config.session_store :cookie_store, :key => '_foursquare-speed_session'
+FoursquareSpeed::Application.config.session_store :cookie_store, :key => '_so_informed_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
