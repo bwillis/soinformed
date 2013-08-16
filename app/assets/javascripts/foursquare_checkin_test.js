d41868fcb50a563db6ba77f504ce561657443ce9
Ben Willis <benjamin.willis@gmail.com>
2013-08-16 00:07:15 -0700

    Stripping out new lines and extra spaces in push test

--- app/assets/javascripts/foursquare_checkin_test.js
+++ app/assets/javascripts/foursquare_checkin_test.js
@@ -1,7 +1,19 @@
 $(function ($) {
-    $("#push-test").on("ajax:success", function() {
-        alert("Push test sent successfully!");
-    }).bind("ajax:error", function() {
-        alert("Failed push test, check the logs for more info!");
+
+
+    $("#push-test").on("submit", function(){
+        var data = $(this).serializeArray();
+        $.each(data, function(){
+            if(this.name == "checkin") {
+                this.value = this.value.replace(/\r?\n/g, '');
+                this.value = this.value.replace(/\s+/g, ' ');
+            }
+        });
+        $.post($(this).attr("action"), data, function() {
+            alert("Push test sent successfully!");
+        }).fail(function() {
+            alert("Failed push test, check the logs for more info!");
+        });
+        return false;
     });
 });
