44df0c1600217ec0d507036f4a7a5423b6f13958
Ben Willis <benjamin.willis@gmail.com>
2013-08-25 20:35:46 -0700

    fixing args problem!

--- lib/so_informed/foursquare/client_factory.rb
+++ lib/so_informed/foursquare/client_factory.rb
@@ -3,9 +3,9 @@ module SoInformed
     class ClientFactory
       def self.get_client(*args)
         if [:development, :test].include? Rails.env.to_sym
-          LoggerClient.new(args)
+          LoggerClient.new(*args)
         else
-          ::Foursquare::Base.new(args)
+          ::Foursquare::Base.new(*args)
         end
       end
     end
