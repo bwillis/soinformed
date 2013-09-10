8a9ecc7b4a96e78ac6d1fd9e70f6265d4c13fb18
Ben Willis <benjamin.willis@gmail.com>
2013-09-09 21:31:14 -0700

    missing requires for dalli store in initializers

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,5 @@
-# Be sure to restart your server when you modify this file.
+require 'action_dispatch/middleware/session/dalli_store'
 
 session_store_type = Rails.env.production? ? :dalli_store : :cookie_store
 
 SoInformed::Application.config.session_store session_store_type, :key => '_sis'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# SoInformed::Application.config.session_store :active_record_store
