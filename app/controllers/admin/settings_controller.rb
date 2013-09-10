b483c5a0276465ed530de594802e43724b2aa2bb
Ben Willis <benjamin.willis@gmail.com>
2013-09-09 20:36:24 -0700

    adding application properties display

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,6 +1,10 @@
 module Admin
   class SettingsController < Admin::BaseController
 
+    def index
+      @application = SoInformed::AppFactory.get_application
+    end
+
     def create
       set_dev_session_enabled(params[:dev_session_enabled])
       if user_session.authenticated?
