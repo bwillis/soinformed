29c649ef78a2caaf68da9f79d858df4c83971052
Ben Willis <benjamin.willis@gmail.com>
2014-05-20 11:05:25 -0700

    hide the phone number from the logs

--- config/application.rb
+++ config/application.rb
@@ -43,7 +43,7 @@ module SoInformed
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :phone_number]
 
     # keep alive
     config.middleware.use "Ping"
